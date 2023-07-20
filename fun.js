4fb04e712e3b6ed11ade0e9913b9f3527d10aa99
AntonP22 <91823229+AntonP22@users.noreply.github.com>
2023-07-20 19:17:19 +0300

    часть2

--- fun.js
+++ fun.js
@@ -1,5 +1,7 @@
 const getNumber = (max = 11, min = 0) =>
   Math.floor(Math.random() * (max - min) + min);
+const optimizeNumber = (n) => (n+"").length === 2 ? n : "0" + n;
+
 
 const data = [];
 let n = getNumber(41, 1);
@@ -8,3 +10,9 @@ while (n--) {
   data.push(getNumber(100))
 }
 console.log(data);
+
+const box = document.querySelector('.box');
+
+for (let val of data) {
+  box.innerHTML += `<div class="card">${optimizeNumber(val)}</div>`;
+ }
